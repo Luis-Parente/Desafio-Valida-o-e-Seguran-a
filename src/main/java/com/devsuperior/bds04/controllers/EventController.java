2dde454abdcf6c290b5c4c5e629cc0d5ee4ca17e
Luis Parente <lgsp25@hotmail.com>
2025-04-28 19:13:45 -0300

    Testes ok

--- src/main/java/com/devsuperior/bds04/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -18,6 +18,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.devsuperior.bds04.dto.EventDTO;
 import com.devsuperior.bds04.services.EventService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/events")
 public class EventController {
@@ -34,7 +36,7 @@ public class EventController {
 	
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PostMapping
-	public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+	public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
